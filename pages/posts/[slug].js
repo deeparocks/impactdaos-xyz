2d91a086b76fefa9112312ea2a1eff11643761dc
deeparocks <99833029+deeparocks@users.noreply.github.com>
2022-11-23 17:16:03 -0800

    bottom nav

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { getPostBySlug, getAllPosts } from '../../lib/posts'
 import { markdownToHtml} from "../../lib/markdown"
 import { Layout } from "../../components/Layout"
@@ -12,13 +13,42 @@ export default function Post({ post }) {
         <div className="w-full md:w-6/12">
          <article className="prose prose-zinc" 
          dangerouslySetInnerHTML={{__html: post.content}} />
-         </div>
+        </div>
+        <Nav post={post} />
     </Layout>
   )
 }
 
+const Nav = ({post}) => (
+  <div className="flex gap-2">
+    {post.previous && <NavButton post={post.previous} label="Previous" />}
+    {post.next && <NavButton post={post.next} label="Next" />}
+  </div>
+)
+
+const NavButton = ({post, label}) => (
+  <Link href={post.slug}>
+    <div className="border rounded-full px-6 py-2 text-center cursor-pointer hover:border-black">
+      <div className="text-green-700 font-bold">{label}</div>
+      <div className="text-sm">{post.title}</div>
+    </div>
+  </Link>
+)
+
 export async function getStaticProps({ params }) {
-  const post = getPostBySlug(params.slug, [
+  const posts = getAllPosts(['slug', 'title'])
+  const nl = posts.filter((p, i) => (
+      posts[i+1]?.slug === params.slug || posts[i-1]?.slug === params.slug 
+      || p.slug === params.slug
+  )) 
+  const nav = (
+    nl.length === 3 ? { previous: nl[0], next: nl[2]} : // if 
+    nl[0].slug === params.slug ? { previous: null, next: nl[1]} : // else if 
+    nl[1].slug === params.slug ? { previous: nl[0], next: null} : // else if
+    null // else 
+  );
+
+  const post = {...getPostBySlug(params.slug, [
     'title',
     'description',
     'date',
@@ -26,7 +56,7 @@ export async function getStaticProps({ params }) {
     'author',
     'content',
     'image',
-  ])
+  ]), ...nav}
 
   const content = await markdownToHtml(post.content || '')
 
